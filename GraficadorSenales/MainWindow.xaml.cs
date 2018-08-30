d9417a8197031cfe73f5e878eddeedcc93330876
YoSoyTonny <itonygame@gmail.com>
2018-08-30 10:40:37 -0700

    Rampa

--- GraficadorSenales/MainWindow.xaml.cs
+++ GraficadorSenales/MainWindow.xaml.cs
@@ -69,5 +69,36 @@ namespace GraficadorSenales
             }
 
         }
+
+        private void btnGraficarRampa_Click(object sender, RoutedEventArgs e)
+        {
+            double TiempoInicial = double.Parse(txtTiempoInicial.Text);
+            double TiempoFinal = double.Parse(txtTiempoFinal.Text);
+            double FrecuenciaMuestreo = double.Parse(txtFrecuenciaMuestreo.Text);
+
+            SenalRampa señal = new SenalRampa();
+
+            double periodoMuestreo = 1 / FrecuenciaMuestreo;
+
+            for (double i = TiempoInicial; i <= TiempoFinal; i += periodoMuestreo)
+            {
+                double valorMuestra = señal.evaluar(i);
+
+                if (Math.Abs(valorMuestra) > señal.AmplitudMaxima)
+                {
+                    señal.AmplitudMaxima = Math.Abs(valorMuestra);
+                }
+
+                señal.Muestra.Add(new Muestra(i, valorMuestra));
+            }
+
+            foreach (Muestra muestra in señal.Muestra)
+            {
+                plnGrafica.Points.Add(new Point(muestra.X * scrContenedor.Width
+                    , (muestra.Y * ((scrContenedor.Height / 2.0) - 30) * -1)
+                    + (scrContenedor.Height / 2))
+                    );
+            }
+        }
     }
 }
---
+++ GraficadorSenales/SenalRampa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSenales
+{
+    class SenalRampa
+    {
+        public List<Muestra> Muestra { get; set; }
+        public double AmplitudMaxima { get; set; }
+
+        public SenalRampa()
+        {
+            Muestra = new List<Muestra>();
+            AmplitudMaxima = 0.0;
+        }
+
+        public SenalRampa(double amplitud, double fase, double frecuencia)
+        {
+            Muestra = new List<Muestra>();
+            AmplitudMaxima = 0.0;
+        }
+
+        public double evaluar(double tiempo)
+        {
+            double resultado;
+            
+            if (tiempo >= 0)
+            {
+                resultado = tiempo;
+            } else
+            {
+                resultado = 0;
+            }
+
+            return resultado;
+
+
+        }
+
+    }
+}
